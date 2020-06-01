e2fd90ad206cb02ca4c2f6fa95e816fd383dad2a
lc9312 <742875884@qq.com>
2020-06-01 21:08:14 +0800

    Drinks 4th

--- git_test/src/com/itheima/git/Drinks.java
+++ git_test/src/com/itheima/git/Drinks.java
@@ -4,5 +4,6 @@ public class Drinks {
     public static void main(String[] args) {
         System.out.println("1板爽歪歪");
         System.out.println("2板爽歪歪");
+        System.out.println("3板爽歪歪");
     }
 }
